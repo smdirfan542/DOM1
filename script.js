affd15e17dcfac9fb3643a19bfc366e59bfc5f0c
Shaik Mahammed Irfan <smdirfan542@email.com>
2025-12-02 23:40:22 +0530

    button random text

--- script.js
+++ script.js
@@ -1,68 +1,21 @@
-var h=document.querySelector('.helem');
-// h.addEventListener('click',function(){
-//     h.innerHTML="irfan"
-//     h.style.color='yellow'
-//     h.style.backgroundColor='black'
-//     h.style.width='fit-content'
-// })
-
-h.addEventListener('click',function(){
-    var a=Math.random()*10;
-    
-    h.innerHTML="Random:-"+a+"  Floor:-"+Math.floor(a);
-})
-
-var box=document.querySelector('#container1');
+var main=document.querySelector('.main');
 var btn=document.querySelector('#btn');
 btn.addEventListener('click',function(){
-    
-        var c1=Math.floor(Math.random()*245);
-        var c2=Math.floor(Math.random()*245);
-        var c3=Math.floor(Math.random()*245);
-        //array one
-        var arr=['irfan','zahid','rehan','faizan','sayed'];
-        var ct=Math.floor(Math.random()*arr.length);
-         box.innerHTML=arr[ct]+" "+arr[ct+1];
-        box.style.backgroundColor=`rgb(${c1},${c2},${c3})`;
+    var h=document.createElement('h1');
+    h.style.color='white';
+    var x=Math.random()*80;
+    var y=Math.random()*80;
+    var deg=Math.random()*360;
+    var scal=Math.floor(Math.random()*3);
+    var headings=['iam irfan','iam smd','doing sce','iam sde bca','mca'];
+    var a=Math.floor(Math.random()*headings.length);
+    h.innerHTML=headings[a];
+    h.style.fontSize='40px'
+    h.style.textWrap='unwrap'
+    h.style.left=x+'%';
+    h.style.top=y+'%';
+    h.style.rotate=deg+'deg'
+    h.style.scale=scal>0?scal:1;
+    h.style.position='absolute';
+    main.appendChild(h);
 })
-
-var teams=[
-    {
-        team:"csk",
-        Color:"yellow",
-        secColor:"gold"
-    },{
-        team:"rcb",
-        Color:"black",
-        secColor:"red"
-    }
-    ,{
-        team:"srh",
-        Color:"orange",
-        secColor:"black"
-    }
-    ,{
-        team:"mi",
-        Color:"blue",
-        secColor:"gold"
-    }
-    ,{
-        team:"kX11",
-        Color:"crimson",
-        secColor:"black"
-    }
-]
-var iplh=document.querySelector('#iplh');
-var iplbtn=document.querySelector('#iplbtn');
-var iplground=document.querySelector('.main');
-var iplsecground=document.querySelector('.second');
-iplbtn.addEventListener('click',function(){
-    // iplh.innerHTML=teams[Math.floor(Math.random()*10)].team;
-    var a=Math.floor(Math.random()*teams.length);
-    var team=teams[a];
-    iplh.innerHTML=team.team;
-    iplh.style.color='white'
-    iplground.style.backgroundColor=team.Color;
-    iplsecground.style.backgroundColor=team.secColor;
-})
-
